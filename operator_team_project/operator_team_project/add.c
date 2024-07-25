62ccf48db0f229d9ef46cd933a2985009dfaaecc
standbyme227 <standbyme227@gmail.com>
2024-07-25 17:20:54 +0900

    Add Function c8
    함수 주석 처리 및 거꾸로 재배열 전에 \n값을 처리해서 문자열의 끝을 알 수 있도록 처리

--- operator_team_project/operator_team_project/add.c
+++ operator_team_project/operator_team_project/add.c
@@ -8,26 +8,26 @@ void removeSign(char *str)
     memmove(str, str + 1, strlen(str));
 }
 
-// void removeZeros(char *str)
-// {
-//     // 문자열 앞에 있는 0을 지운다.
-//     int index = 0;
-//     while (str[index] == '0')
-//     {
-//         index++;
-//     }
+void removeZeros(char *str)
+{
+    // 문자열 앞에 있는 0을 지운다.
+    int index = 0;
+    while (str[index] == '0')
+    {
+        index++;
+    }
 
-//     // index가 0이 아니라면, 앞에 있는 0을 제거
-//     if (index > 0)
-//     {
-//         int i = 0;
-//         while (str[index])
-//         {
-//             str[i++] = str[index++];
-//         }
-//         str[i] = '\0'; // 문자열 끝에 null 문자 추가
-//     }
-// }
+    // index가 0이 아니라면, 앞에 있는 0을 제거
+    if (index > 0)
+    {
+        int i = 0;
+        while (str[index])
+        {
+            str[i++] = str[index++];
+        }
+        str[i] = '\0'; // 문자열 끝에 null 문자 추가
+    }
+}
 
 // 그니까....
 // 생각해보니
@@ -50,8 +50,7 @@ void reversedString(const char *origin_char, char *result)
 
 int charToInt(char c)
 {
-    // 문자값 확인
-    // printf("%c\n", c);
+    // 문자를 정수로 변환하는 부분
     if (c >= '0' && c <= '9')
     {
         return c - '0';
@@ -66,8 +65,16 @@ int charToInt(char c)
 
 char *getAdd(char *first, char *second)
 {
-    // 문자열을 거꾸로 순환한다.
-
+    // 문자열 2개를 순환하면서 값을 더한다.
+    // 문자열들을 거꾸로 재배열한다.
+    // 순환하면서 1의 자리부터 덧셈을 처리한다.
+    // (가장 처음에는 \n값을 집어넣는다.)
+    // 덧셈처리는 해당 문자값을 정수로 바꾼뒤 처리한다.
+    // 만약 자리 올림값이 생기면 다음 자리수 계산에 추가하여 같이한다.
+    // 결과값은 원하는 값이 거꾸로된 문자열이다.
+    // 부호를 추가한다.
+    // 해당값을 다시 거꾸로 재배열한다.
+    // 재배열 후 출력한다.
     char sign = first[0];
 
     if (first[0] != second[0])
@@ -76,27 +83,34 @@ char *getAdd(char *first, char *second)
         return NULL;
     }
 
-    char* first_copy = _strdup(first);
-    char* second_copy = _strdup(second);
+    // 문자열 복사
+    char *first_copy = strdup(first);
+    char *second_copy = strdup(second);
 
     // 부호삭제
     removeSign(first_copy);
     removeSign(second_copy);
 
-    int first_length =(int)strlen(first_copy), second_length = (int)strlen(second_copy);
+    // 문자열 길이
+    int first_length = (int)strlen(first_copy), second_length = (int)strlen(second_copy);
+
+    // 가장 긴 문자열 길이
     int length = (first_length > second_length) ? first_length : second_length;
+    length++; // \n값을 위한 처리
+
+    // 자리 올림 값
     int carry_in = 0;
 
+    // 두 문자열 거꾸로 재배열
     char *reversed_first = malloc(first_length + 2);
     char *reversed_second = malloc(second_length + 2);
     memset(reversed_first, 0, first_length + 2);
     memset(reversed_second, 0, second_length + 2);
-    // char reversed_first[first_length + 2];
-    // char reversed_second[second_length + 2];
 
     reversedString(first_copy, reversed_first);
     reversedString(second_copy, reversed_second);
 
+    // 문자열 합 계산
     char first_char_value = 0, second_char_value = 0;
     int result = 0;
     int sum = 0;
@@ -105,6 +119,10 @@ char *getAdd(char *first, char *second)
 
     for (int i = 0; i <= length; i++)
     {
+        if (i == 0)
+        {
+            results[0] = '\n';
+        }
         int first_value = 0, second_value = 0;
 
         if (i <= first_length - 1)
@@ -130,15 +148,18 @@ char *getAdd(char *first, char *second)
             carry_in = 0;
             result = sum;
         }
-        results[i] = result + '0';
+        results[i + 1] = result + '0';
     }
 
-    char *reversed_results = malloc(length + 4);
+    // 다시 거꾸로 재배열 처리 준비
+    char *reversed_results = malloc(length + 2);
+    memset(reversed_results, 0, length + 2);
 
+    // 부호 추가
     results[length] = sign;
 
     reversedString(results, reversed_results);
-    // removeZeros(reversed_results);
+    removeZeros(reversed_results);
 
     // printf("Input A : %s\n", first);
     // printf("Input B : %s\n", second);
